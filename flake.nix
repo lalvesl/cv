bd26d9d0b9cde2ccbd2a681a52989c01d33e848b
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2025-06-15 20:48:10 -0300

    ci-cd(nix): add flake with development,watch and build

---
+++ flake.nix
@@ -0,0 +1,101 @@
+{
+  description = "abnTeX2 environment with TeX Live";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      for_each_system = nixpkgs.lib.genAttrs supportedSystems;
+
+      pkgs = for_each_system (system: import nixpkgs { inherit system; });
+      tex_pkgs = for_each_system (
+        system:
+        pkgs.${system}.texlive.combine {
+          inherit (pkgs.${system}.texlive) scheme-full abntex2;
+        }
+      );
+    in
+    {
+      devShells = for_each_system (system: {
+        default = pkgs.${system}.mkShell {
+          name = "tex_in_live shell";
+          buildInputs = [
+            tex_pkgs.${system}
+          ];
+        };
+      });
+
+      apps = for_each_system (system: {
+        default = {
+          type = "app";
+          program = "${pkgs.${system}.writeShellScript "watch" ''
+            echo "Watching for changes..."
+            mkdir -p result_watch
+            if ! pgrep -x "zathura" > /dev/null; then
+              ${pkgs.${system}.zathura}/bin/zathura result_watch/main.pdf &
+            fi
+            exec ${tex_pkgs.${system}}/bin/latexmk -pvc -pdf -outdir=result_watch main.tex
+            find | grep "eps-converted-to.pdf" | xargs rm
+          ''}";
+        };
+
+        clear = {
+          type = "app";
+          program = "${pkgs.${system}.writeShellScript "clear" ''
+            echo "Remove generated files"
+            git ls-files --others -i --exclude-standard | xargs rm -r
+            rm -r result*
+          ''}";
+        };
+      });
+
+      # packages = for_each_system (system: {
+      #   default = pkgs.${system}.writeShellScriptBin "latex-build" ''
+      #     rm -r result
+      #     mkdir -p result
+      #     exec ${tex_pkgs.${system}}/bin/latexmk -pdf -outdir=result main.tex
+      #   '';
+      # });
+
+      packages = for_each_system (system: {
+        default = pkgs.${system}.stdenv.mkDerivation {
+          pname = "abntex2_some_document";
+          version = "1.0";
+
+          src = ./.;
+
+          buildInputs = [ tex_pkgs.${system} ];
+
+          buildPhase = ''
+            echo "📦 Building your document..."
+            mkdir -p $out
+            cp -r $src/* .
+            latexmk -pdf main.tex
+            echo "✅ Build complete: main.pdf"
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp main.pdf $out/lucas_alves_de_lima.pdf
+          '';
+
+          # dontFixup = true;
+          # unpackPhase = "true";
+        };
+      });
+    };
+}
